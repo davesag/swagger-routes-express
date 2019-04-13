b5d54abb48c72d59125c1652b8598f80c1794d30
Dave Sag <davesag@gmail.com>
2019-04-13 14:01:13 +0530

    n/a fixed some linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
   extends: ['standard', 'prettier', 'prettier/standard'],
-  plugins: ['prettier', 'standard', 'mocha'],
+  plugins: ['prettier', 'standard', 'import'],
   parserOptions: {
     sourceType: 'module'
   },
   env: {
     es6: true,
-    node: true,
-    mocha: true
+    node: true
   },
   rules: {
     'prettier/prettier': ['error', { singleQuote: true, semi: false }],
