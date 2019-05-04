4a10d0eedf5bf02cee092d03deaa1c7a7ec0dea5
Dave Sag <davesag@gmail.com>
2019-05-04 09:44:50 +0530

    n/a updated dependencies and moved test helper

--- stryker.conf.js
+++ stryker.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
     testRunner: 'mocha',
     mochaOptions: {
       spec: ['./test/unit/**/*.test.js'],
-      require: ['./test/unit/testHelper.js']
+      require: ['./test/unitTestHelper.js']
     },
     transpilers: [],
     testFramework: 'mocha',
