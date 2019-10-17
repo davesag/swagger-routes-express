b547d6a2a6a23fe360f29d86c62958fce39a94ab
davesag <davesag@gmail.com>
2019-10-17 09:41:31 +0530

    n/a added comment with link to docs

--- src/normalise/v2/normaliseSecurity.js
+++ src/normalise/v2/normaliseSecurity.js
@@ -1,3 +1,4 @@
+// ref https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#securityRequirementObject
 const normaliseSecurity = security =>
   security
     ? Object.values(security[0])[0]
