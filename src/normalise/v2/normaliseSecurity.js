59cc30f276734398fcf4257189832f84420148e7
davesag <davesag@gmail.com>
2019-10-19 08:16:50 +0530

    #68 added comments

--- src/normalise/v2/normaliseSecurity.js
+++ src/normalise/v2/normaliseSecurity.js
@@ -1,4 +1,12 @@
 // ref https://github.com/OAI/OpenAPI-Specification/blob/master/versions/2.0.md#securityRequirementObject
+
+/**
+ *  Normalises the supplied security object into a string.
+ *
+ * @param security — The security object, or undefined
+ * @returns a string representation of the security, or undefined if the
+ *          security was undefined
+ */
 const normaliseSecurity = security =>
   security
     ? Object.values(security[0])[0]
